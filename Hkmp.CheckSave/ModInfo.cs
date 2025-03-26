4f1e10d94044021b45c1134d314183d05b51626f
MorozicH20 <125402976+MorozicH20@users.noreply.github.com>
2025-03-26 19:14:14 +0300

    preparing for the release

--- Hkmp.CheckSave/ModInfo.cs
+++ Hkmp.CheckSave/ModInfo.cs
@@ -3,6 +3,6 @@
     internal class ModInfo
     {
         public const string Name = "HKMP.CheckSave";
-        public const string Version = "0.1.0.0";
+        public const string Version = "1.0.0.0";
     }
 }
